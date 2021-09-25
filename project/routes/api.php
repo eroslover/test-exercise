fa0fab5cb1d8f6d3b92bfb7836f1bec20a628f01
Yaroslav Shikovets <blackzik@mail.ru>
2021-09-26 00:11:54 +0600

    Remove extra routes

--- project/routes/api.php
+++ project/routes/api.php
@@ -28,6 +28,3 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
