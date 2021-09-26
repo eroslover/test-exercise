86fcf05d2cc20f5bb8855ce0c5cc455b0d0ad982
Yaroslav Shikovets <blackzik@mail.ru>
2021-09-26 21:38:01 +0600

    Update test script

--- project/test.sh
+++ project/test.sh
@@ -1,4 +1,2 @@
-#php artisan migrate:refresh --seed --env=testing
-
+php artisan migrate:refresh --seed --env=testing
 vendor/bin/codecept run api
-#vendor/bin/codecept run unit modules/Promocodes/Tests/PromocodesTest
